51e6a29ebfbf02b04bf51f229d380f74d545ddcc
DarkLegend360 <darklegend360@live.com>
2020-06-10 07:49:37 +0530

    Updates

---
+++ precache-manifest.54eeebd1274bea8555db6a28e230205f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "796fef5e1658eb6845f7f611a30aced0",
+    "url": "/Injustice_Society/index.html"
+  },
+  {
+    "revision": "a1c5eb01c77bbde730c4",
+    "url": "/Injustice_Society/static/css/main.d51635d1.chunk.css"
+  },
+  {
+    "revision": "af0603c39cb9d18fe5f8",
+    "url": "/Injustice_Society/static/js/2.5a78173e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Injustice_Society/static/js/2.5a78173e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1c5eb01c77bbde730c4",
+    "url": "/Injustice_Society/static/js/main.ddda239c.chunk.js"
+  },
+  {
+    "revision": "11afa47f8de85855df29",
+    "url": "/Injustice_Society/static/js/runtime-main.a3031fe9.js"
+  }
+]);
